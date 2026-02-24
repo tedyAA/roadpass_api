ed3251d58a88a7ed8e880089e71789ad843d0bda
Teodora Angelova <43522056+tedyAA@users.noreply.github.com>
2026-02-24 17:02:07 +0200

    prettier

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -2,26 +2,33 @@ class TripsController < ApplicationController
   def index
     @trips = Trip.all
 
-    @trips = @trips.where("LOWER(name) LIKE ?", "%#{params[:search].to_s.downcase}%") if params[:search].present?
-    @trips = @trips.where("rating >= ?", params[:min_rating].to_i) if params[:min_rating].present?
+    if params[:search].present?
+      @trips = @trips.where("LOWER(name) LIKE ?", "%#{params[:search].to_s.downcase}%")
+    end
+
+    if params[:min_rating].present?
+      @trips = @trips.where("rating >= ?", params[:min_rating].to_i)
+    end
+
     sort_column, sort_order = if params[:sort].present? && %w[asc desc].include?(params[:sort].downcase)
-                                ['rating', params[:sort]]
-                              else
-                                ['name', 'asc']
-                              end
+                               ['rating', params[:sort].downcase]
+                             else
+                               ['name', 'asc']
+                             end
+
     @trips = @trips.order("#{sort_column} #{sort_order}")
-    @trips = @trips.page(params[:page]).per(params[:per_page] || 10)
+                   .page(params[:page])
+                   .per(params[:per_page] || 10)
 
     render :index
   end
-   def show
-  @trip = Trip.find_by(id: params[:id])
-  unless @trip
-    redirect_to trips_path, alert: "Trip not found"
-  end
-end
 
-def create
+  def show
+    @trip = Trip.find_by(id: params[:id])
+    return redirect_to trips_path, alert: "Trip not found" unless @trip
+  end
+
+  def create
     @trip = Trip.new(trip_params)
 
     if @trip.save
