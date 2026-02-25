d59c2ce03bbbe03307fc2f45f077fae445696c6e
Teodora Angelova <43522056+tedyAA@users.noreply.github.com>
2026-02-25 14:49:38 +0200

    refactor

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3001'  # your React dev server
+    origins 'http://localhost:5173'  # your React dev server
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
