d26105b15123245734485b5e1cb8c10c19ddbc13
Teodora Angelova <43522056+tedyAA@users.noreply.github.com>
2026-02-27 16:55:48 +0200

    refactored model to better

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,4 +1,11 @@
 class Trip < ApplicationRecord
-   validates :name, :image_url, :short_description, presence: true
-  validates :rating, inclusion: { in: 1..5 }
+  validates :name, presence: true
+  validates :image_url, presence: true
+  validates :short_description, presence: true
+  validates :long_description, presence: true
+
+  validates :rating,
+            presence: true,
+            numericality: { only_integer: true },
+            inclusion: { in: 1..5 }
 end
