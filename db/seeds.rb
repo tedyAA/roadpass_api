172ab40daa2592320d4d9c0344fa8a034e137998
Teodora Angelova <43522056+tedyAA@users.noreply.github.com>
2026-02-27 16:55:57 +0200

    refactored seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,24 @@
 require 'json'
 
-# Clear existing trips to avoid duplicates
-Trip.destroy_all
-
-# Load JSON file
 file_path = Rails.root.join('db', 'data.json')
 json_data = File.read(file_path)
 data = JSON.parse(json_data)
 
-# Create trips from JSON
-data['trips'].each do |trip|
-  Trip.create!(
-    name: trip['name'],
-    image_url: trip['image_url'],
-    short_description: trip['short_description'],
-    long_description: trip['long_description'],
-    rating: trip['rating']
-  )
+unless data['trips'].is_a?(Array)
+  raise "Invalid JSON format: expected 'trips' array"
+end
+
+Trip.transaction do
+  data['trips'].each do |trip|
+    Trip.find_or_create_by!(name: trip['name']) do |t|
+      t.assign_attributes(
+        image_url: trip['image_url'],
+        short_description: trip['short_description'],
+        long_description: trip['long_description'],
+        rating: trip['rating']
+      )
+    end
+  end
 end
 
 puts "Seeded #{Trip.count} trips!"
